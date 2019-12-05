9df0a908ec2dda5d99322874ae0ac8fd56edd8eb
nkfoss <nathankfoss@gmail.com>
2019-12-05 11:23:04 -0600

    Implement packet receiving with debug loop
    The packet receiving is governed by a loop that cuts off at 500
    packets. Then a debug loop is started to probe the packets.
    Commands for the debug are inside the code.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -2,6 +2,8 @@ package segmentedfilesystem;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,10 +16,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         // Check correct number args
-        if (args.length != -1) {
-            System.out.println("Usage: java QuoteClient <hostname>");
-            return;
-        }
+//        if (args.length != -1) {
+//            System.out.println("Usage: java QuoteClient");
+//            return;
+//        }
 
 
         try
@@ -25,21 +27,68 @@ public class Main {
             // Create the DG-socket
             DatagramSocket socket = new DatagramSocket();
             byte[] buf = new byte[256];
-            InetAddress address = InetAddress.getByName(args[0]);
+            InetAddress address = InetAddress.getByName("csci-4409.morris.umn.edu");
 
             // Note: the DG-packet contains an empty byte-array (buf). This is fine when requesting a response.
             //     all that the server needs is the address/port number (which is automatically added to the packet).
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
             socket.send(packet);
 
-            // Receiving the packet
-            packet = new DatagramPacket(buf, buf.length);
-            socket.receive(packet);
-            String received = new String(packet.getData(), 0, packet.getLength());
-            System.out.println("Quote of the moment: "  + received);
+            // Receiving the packets
+            int count = 0;
+            ArrayList<DatagramPacket> packets = new ArrayList<DatagramPacket>(1);
+            System.out.println("Receiving started...");
+            while ( count < 500 ) {
+                count++;
+                packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                packets.add(packet);
+
+
+                if (packet.getLength() < 256) {
+                    System.out.println("End packet found");
+                    System.out.println("Count: " + count);
+
+                }
+
+            }
+            System.out.println("Done receiving!");
+
+            Scanner sn = new Scanner(System.in);
+            boolean debug = true;
+            while (debug) {
+                System.out.println("Starting debug...please enter command");
+                String input = sn.next();
+                switch(input) {
+                    case "pall":
+                        for (DatagramPacket pack : packets) {
+                            System.out.println( pack.getData()[0] );
+                        }
+                        break;
+                    case "p":
+                        System.out.println("enter index to query");
+                        int index = sn.nextInt();
+                        try {
+                            System.out.println("Print first and second bit");
+                            System.out.println(packets.get(index).getData()[7]);
+                            System.out.println(packets.get(index).getData()[6]);
+                            break;
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                            break;
+                        }
+                    case "quit":
+                        System.out.println("leaving...");
+                        debug = false;
+                        break;
+                }
+
+            }
 
         }
         catch (SocketException e) { e.printStackTrace(); }
+
+
 
 
 
