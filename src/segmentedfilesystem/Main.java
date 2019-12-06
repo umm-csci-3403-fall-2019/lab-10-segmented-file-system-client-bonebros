eaa6bfb4ac3f3eaaf29d4b684c2c6c3a6f283671
nkfoss <nathankfoss@gmail.com>
2019-12-05 20:25:21 -0600

    Implement new debug probing and PacketData implementation

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -15,87 +15,118 @@ public class Main {
     
     public static void main(String[] args) throws IOException {
 
-        // Check correct number args
-//        if (args.length != -1) {
-//            System.out.println("Usage: java QuoteClient");
-//            return;
-//        }
+        // Create the DG-socket
+        DatagramSocket socket = new DatagramSocket();
+        byte[] buf = new byte[256];
+        InetAddress address = InetAddress.getByName("csci-4409.morris.umn.edu");
 
+        // Note: the DG-packet contains an empty byte-array (buf). This is fine when requesting a response.
+        //     all that the server needs is the address/port number (which is automatically added to the packet).
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
+        socket.send(packet);
 
-        try
-        {
-            // Create the DG-socket
-            DatagramSocket socket = new DatagramSocket();
-            byte[] buf = new byte[256];
-            InetAddress address = InetAddress.getByName("csci-4409.morris.umn.edu");
+        // Receiving the packets
+        System.out.println("Receiving started...");
+        ArrayList<PacketData> allPacketData = new ArrayList<PacketData>(1);
 
-            // Note: the DG-packet contains an empty byte-array (buf). This is fine when requesting a response.
-            //     all that the server needs is the address/port number (which is automatically added to the packet).
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
-            socket.send(packet);
+        int count = 0;
+        while ( count < 553 ) {
+            System.out.println(count);
+            count++;
 
-            // Receiving the packets
-            int count = 0;
-            ArrayList<DatagramPacket> packets = new ArrayList<DatagramPacket>(1);
-            System.out.println("Receiving started...");
-            while ( count < 500 ) {
-                count++;
-                packet = new DatagramPacket(buf, buf.length);
-                socket.receive(packet);
-                packets.add(packet);
+            packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
 
+            PacketData packetData = new PacketData(packet.getData());
+            allPacketData.add(packetData);
+        }
 
-                if (packet.getLength() < 256) {
-                    System.out.println("End packet found");
-                    System.out.println("Count: " + count);
+        System.out.println("Done receiving!");
+        System.out.println("Start debug? <enter 'y' to start>");
 
-                }
+        Scanner mainScan = new Scanner(System.in);
+        String debug = mainScan.next();
 
+        if (debug.equals("y")) { debug(allPacketData); }
+        else {return;}
+    }
+
+    public static void debug(ArrayList<PacketData> allPacketData) {
+        System.out.println("Debug started...please enter command");
+        Scanner sn = new Scanner(System.in);
+        while (true) {
+            String input = sn.next();
+            switch(input) {
+                case "p":
+                    System.out.println("enter index of PacketData to query");
+                    int index = sn.nextInt();
+                    queryPacket( allPacketData.get(index) );
+                    break;
+//                case "pall":
+//                    pAll(allPacketData);
+//                    break;
+                case "q":
+                    System.out.println("leaving...");
+                    return;
             }
-            System.out.println("Done receiving!");
-
-            Scanner sn = new Scanner(System.in);
-            boolean debug = true;
-            while (debug) {
-                System.out.println("Starting debug...please enter command");
-                String input = sn.next();
-                switch(input) {
-                    case "pall":
-                        for (DatagramPacket pack : packets) {
-                            System.out.println( pack.getData()[0] );
-                        }
-                        break;
-                    case "p":
-                        System.out.println("enter index to query");
-                        int index = sn.nextInt();
-                        try {
-                            System.out.println("Print first and second bit");
-                            System.out.println(packets.get(index).getData()[7]);
-                            System.out.println(packets.get(index).getData()[6]);
-                            break;
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                            break;
-                        }
-                    case "quit":
-                        System.out.println("leaving...");
-                        debug = false;
-                        break;
-                }
-
-            }
+            System.out.println("Anything else?");
 
         }
-        catch (SocketException e) { e.printStackTrace(); }
-
-
-
-
-
-
-
-
 
     }
+
+    public static void queryPacket(PacketData packetData) {
+
+        System.out.println("Enter name of field to query.");
+        Scanner sn1 = new Scanner(System.in);
+        while (true) {
+            String input = sn1.next();
+            if (input.equals("q")) {
+                System.out.println("Returning to top...");
+                return;
+            }
+            else if (input.equals("h")) {
+                System.out.println( "commands: id, last, header, num");
+            }
+            else if (input.equals("id")) {
+                System.out.println( packetData.fileId);
+            }
+            else if (input.equals("last")) {
+                System.out.println( packetData.last);
+            }
+            else if (input.equals("header")) {
+                System.out.println( packetData.header);
+            }
+            else if (input.equals("num")) {
+                System.out.println( packetData.packetNumber );
+            }
+
+            System.out.println("Enter another index? (Enter 'q' to return to DEBUG ... 'h' for help)");
+        }
+    }
+
+//    public static void pAll(ArrayList<PacketData> packetDatas) {
+//
+//        System.out.println("Enter the index of the byte-arrays (to print for all)");
+//        Scanner sn1 = new Scanner(System.in);
+//        while (true) {
+//
+//            String input = sn1.next();
+//            if (input.equals("q")) {
+//                System.out.println("Returning to top...");
+//                return;
+//            }
+//            else {
+//                for (int i = 0; i < packets.size(); i++) {
+//                    System.out.println( packets.get(i).getData()[ Integer.parseInt(input) ] );
+//                }
+//            }
+//            System.out.println("Enter another index?");
+//        }
+//
+//
+//    }
+
+
 
 }
